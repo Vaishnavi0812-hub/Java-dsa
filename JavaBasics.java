13be046a5c4902fea680445a3a51cd3533149543
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-05-14 21:24:16 +0530

    Initial commit

---
+++ JavaBasics.java
@@ -0,0 +1,119 @@
+//boilerplate code
+/*import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(num);*/
+
+        //String input=sc.nextLine();
+        //System.out.println(input);
+
+        /*System.out.println("Hello World!"); //line space
+        System.out.print("Hello World!\n");
+        System.out.println("Hello World!");*/
+
+        /*int a=10;
+        String name="Vaishnavi";
+        System.out.println(a);
+        System.out.println(name);*/
+
+        /*byte b=8;
+        System.out.println(b);
+        char ch='a';
+        System.out.println(ch);
+        boolean var= true;
+        System.out.println(var);
+        float price=10.5;
+        int number=25;*/
+
+        /*int a=10;
+        int b=5;
+        int sum=a+b;
+        System.out.println(sum);*/
+
+/*import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float rad=sc.nextFloat();
+        float area=3.14f*rad*rad;
+        System.out.println(area);
+
+        /*int a=sc.nextInt();
+        int b=sc.nextInt();
+        int product=a*b;
+        System.out.println(product);*/
+
+        /*import java.util.*;
+        public class JavaBasics{
+            public static void main(String args[]){
+        
+                int a=25;
+                float b=a;
+                System.out.println(b);*/
+
+                //public class JavaBasics{
+                    //public static  void main(String args[]){
+                        //float a= 12.45f;
+                        //int b = (int) a;
+                        //System.out.println(b);
+
+                        //char ch='a';
+                        //int num=ch;
+                        //System.out.println(num);
+
+                        //char a='a';
+                        //char b='b';
+                        //System.out.println(b-a);
+                       
+                        /*int a=10;
+                        float b=12.50f;
+                        long c=20;
+                        double d=30;
+                        int sum=(int)(a+b+c+d);
+                        System.out.println(sum);*/
+
+                        //byte b=5;
+                        //byte c=(byte)(b*2);
+                        //System.out.println(c);
+
+import java.util.*;
+public class Javabasics{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        //int A=sc.nextInt();
+        //int B=sc.nextInt();
+        //int C=sc.nextInt();
+        //int average=(A+B+C)/3;
+        //System.out.println(average);
+
+        //float a=sc.nextFloat();
+        //float area=a*a;
+        //System.out.println(area);
+
+        /*float pencil=sc.nextFloat();
+        float pen=sc.nextFloat();
+        float eraser=sc.nextFloat();
+        float total=pencil+pen+eraser;
+        System.out.println(total);
+        float tax=total+(0.18f*total);
+        System.out.println(tax);*/
+        
+
+
+    }
+}
+
+
+                    
+            
+            
+
+
+        
+            
+
+
+
+    
