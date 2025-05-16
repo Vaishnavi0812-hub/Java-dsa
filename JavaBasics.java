79341a1c699690884c82b800d519a4b5eaf97f45
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-05-16 20:00:11 +0530

    Conditional Statements

--- JavaBasics.java
+++ JavaBasics.java
@@ -78,9 +78,9 @@ public class JavaBasics{
                         //byte c=(byte)(b*2);
                         //System.out.println(c);
 
-import java.util.*;
-public class Javabasics{
-    public static void main(String args[]){
+//import java.util.*;
+//public class Javabasics{
+    //public static void main(String args[]){
         //Scanner sc=new Scanner(System.in);
         //int A=sc.nextInt();
         //int B=sc.nextInt();
@@ -131,13 +131,168 @@ public class Javabasics{
         z /= (x+y);
         System.out.println(x+ ""+y+""+z);*/
 
-        
+        /*int age=12;
+        if(age>=18) {
+            System.out.println("Adult");
+        }
+        if(age>=14 && age<=17){
+            System.out.println("Teenager");
+        }
+        else {
+            System.out.println("Child");
+        }*/
 
+        /*int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }*/
 
+        /*int age=12;
+        if(age>=18) {
+            System.out.println("Adult");
+        }
+        else if(age>=14 && age<=17){
+            System.out.println("Teenager");
+        }
+        else {
+            System.out.println("Child");
+        }*/
 
-        
+        /*int income=sc.nextInt();
+        int tax;
+        if(income<500000){
+               tax=0;
+        }
+        else if(income>500000 && income<1000000){
+            tax=(int) (income*0.2);
+        }
+        else{
+            tax=(int) (income*0.3);
+        }
+
+        System.out.println("Your tax is" + tax);*/
+
+        /*int a=1;
+        int b=38;
+        int c=6;
+        if(a>b && a>c){
+            System.out.println("a is largest");
+        }
+        else if(b>a && b>c){
+            System.out.println("b is largest");
+        }
+        else{
+            System.out.println("c is largest");
+        }*/
+
+        /*int number=5;
+        String type=((number%2)==0)?"even":"odd";
+        System.out.println(type);*/
+
+        //int marks=sc.nextInt();
+        //String type= (marks>=33)?"PASS":"FAIL";
+        //System.out.println(type);
+
+        /*int number=3;
+        switch(number) {
+            case 2: System.out.println("Samosa");
+            break;
+            case 3: System.out.println("Burger");
+            break;
+            case 1: System.out.println("Ice cream");
+            break;
+            default: System.out.println("None");
+
+        }*/
+import java.util.*;
+    public class JavaBasics {
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            /*System.out.println("Enter 'a' ");
+            int a=sc.nextInt();
+            System.out.println("Enter 'b' ");
+            int b=sc.nextInt();
+            System.out.println("Enter operator");
+            char operator=sc.next().charAt(0);
+
+            switch(operator){
+                case '+': System.out.println(a+b);
+                break;
+                case '-': System.out.println(a-b);
+                break;
+                case '*': System.out.println(a*b);
+                break;
+                case '/': System.out.println(a/b);
+                break;
+                case '%': System.out.println(a%b);
+                break;
+                default : System.out.println("Wrong operator");
+            }*/
+
+            /*int  number=sc.nextInt();
+            if(number>=0){
+                System.out.println("Positive");
+            }
+            else {
+                System.out.println("Negative");
+            }*/
+
+            /*double temp=103.5;
+            if(temp>100){
+                System.out.println("You have a fever");
+            }
+            else {
+                System.out.println("You dont have a fever");
+            }*/
+
+            /*System.out.println("Enter week number");
+            int weeknumber=sc.nextInt();
+            switch(weeknumber){
+                case 1: System.out.println("Monday");
+                break;
+                case 2: System.out.println("Tuesday");
+                break;
+                case 3: System.out.println("Wednesday");
+                break;
+                case 4: System.out.println("Thursday");
+                break;
+                case 5: System.out.println("Friday");
+                break;
+                case 6: System.out.println("Saturday");
+                break;
+                case 7: System.out.println("Sunday");
+                break;
+                default: System.out.println("Wrong number");
+            }*/
+
+            /*int year=sc.nextInt();
+            if(year%4==0) {
+                System.out.println("leap year");
+            }
+            else if(year%100==0) {
+                System.out.println("leap year");
+            }
+            else if(year%400==0) {
+                System.out.println("leap year");
+            }
+            else{
+                System.out.println("not a Leap year");
+            }*/
+
+            
+
+        }
     }
-}
+
+
+
+
+        
+    
+
 
 
 
