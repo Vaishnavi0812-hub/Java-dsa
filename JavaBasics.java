7ece5f129110bdac8668094f8d66f468cf7e8d82
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-05-19 23:34:14 +0530

    Practice Problems

--- JavaBasics.java
+++ JavaBasics.java
@@ -451,12 +451,8 @@ import java.util.*;
 
 System.out.println("Factorial is " + fact);*/
 
-             System.out.println("Enter a number");
-             int num=sc.nextInt();
-             for(int i=1; i<=10; i++){
-                    System.out.println(num + "*" + i + "=" + num*i);
-             }
              
+
         }
     }
 
