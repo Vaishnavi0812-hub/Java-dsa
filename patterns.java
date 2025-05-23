d7a1c6928ab9f5a25ead874b9d91b89676f11609
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-05-23 18:28:33 +0530

    Patterns

--- patterns.java
+++ patterns.java
@@ -98,6 +98,19 @@ public class patterns {
             System.out.println();
           }*/
 
+          /*int n=5;
+          for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j || i+j==n+1){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+          }*/
+
           
 
 
