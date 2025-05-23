ee0d7fdae53a407d2cb7875de78c3009fe75888b
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-05-23 17:56:14 +0530

    Patterns

--- patterns.java
+++ patterns.java
@@ -36,6 +36,71 @@ public class patterns {
             System.out.println();
         }*/
         
+        /*for(int i=1;i<=5;i++){
+            for(int j=1; j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+        /*for(int i=1;i<=3;i++){
+            for(int j=1; j<=10;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+
+        /*for(int i=1;i<=5;i++){
+            for(int j=1; j<=5;j++){
+                if(i-j<=0){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }*/
+
+        /*for(int i=1;i<=5;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+        
+          /*for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i+j>=5+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+          }*/
+          /*int n=6;
+          int m=15;
+          for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+          }*/
+
+          /*int n=7;
+          for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i-j<=0){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+          }*/
+
+          
+
+
 
     }
 }
