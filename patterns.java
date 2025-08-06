505fc0b38199d6563d70b27fc814da32eb35839d
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-06 09:10:07 +0530

    functions

--- patterns.java
+++ patterns.java
@@ -294,9 +294,9 @@ public class patterns{
         /*public static int multiply(int a,int b){
             int product=a*b;
             return product;
-        }
+        }/* */
 
-        public static int factorial(int n){
+        /*public static int factorial(int n){
             int fact=1;
             for(int i=1;i<=n;i++){
                 fact=fact*i;
@@ -315,13 +315,35 @@ public class patterns{
             System.out.println("a*b= " + prod);
         }*/
         
-        public static int bincoeff(int n,int r){
-            n fact=fact
+        /*public static int binCoeff(int n,int r){
+            int fact_n=factorial(n);
+            int fact_r=factorial(r);
+            int fact_nmr=factorial(n-r);
+
+            int binCoeff=fact_n/(fact_r*fact_nmr);
+            return binCoeff;
         }
         public static void main(String args[]){
             Scanner sc=new Scanner(System.in);
-            System.out.println(factorial(4));
+            System.out.println(binCoeff(5,2));
+        }*/
+
+        public static int sum(int a,int b){
+            return a+b;
         }
+        public static int sum(int a, int b, int c){
+            return a+b+c;
+        }
+        public static float sum(float a,float b){
+            return a+b;
+        }
+
+        public static void main(String args[]){
+            System.out.println(sum(2,3));
+            System.out.println(sum(3,4,5));
+            System.out.println(sum(3.2f,7.8f));
+        }
+
             
         }
 
