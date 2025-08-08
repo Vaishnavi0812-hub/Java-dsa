78e4cb8c14adf01e56dfa6853fbdf13b083d1b0a
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-08 13:09:46 +0530

    patterns practice

--- patterns.java
+++ patterns.java
@@ -500,9 +500,16 @@ public class patterns{
            }*/
            public class patterns{
             public static void main(String args[]){
-                for(int i=5;i>=1;i--){
-                    for(int j=1;j<=i;j++){
-                        System.out.print(j);
+                int n=5;
+                for(int i=1;i<=n;i++){
+                    for(int j=1;j<=n;j++){
+                        if(i+j>=n+1){
+                            System.out.print(j);  
+                        }
+                        else{
+                            System.out.print(" ");
+                        }
+                        
                     }
                     System.out.println();
                 }
