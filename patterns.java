6ba3cc5be5e84f2009a065b762c0d6d8eff1b4ba
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-09 18:26:15 +0530

    advanced patterns

--- patterns.java
+++ patterns.java
@@ -499,24 +499,45 @@ public class patterns{
 
            }*/
            public class patterns{
-            public static void main(String args[]){
-                int n=5;
-                for(int i=1;i<=n;i++){
-                    for(int j=1;j<=n;j++){
-                        if(i+j>=n+1){
-                            System.out.print(j);  
+            public static void hollowRectangle(int totalRows, int totalCols){
+                //outer loop
+                for(int i=1;i<=totalRows;i++){
+                    for(int j=1;j<=totalCols;j++){
+                        if(i==1||i==totalRows||j==1||j==totalCols){
+                            System.out.print("*");
                         }
                         else{
                             System.out.print(" ");
                         }
-                        
                     }
                     System.out.println();
                 }
-            
-           }
+            }
+            public static void main(String args[]){
+                hollowRectangle(4,5);
 
-           }
+
+                /*for(int i=1;i<=4;i++){
+                    for(int j=1;j<=i;j++){
+                            System.out.print(j);     
+                    }
+                    System.out.println();
+                }*/
+
+                /*for(int i=1;i<=5;i++){
+                    for(int j=5;j>=i;j--){                             
+                        System.out.print(j);
+                    }
+                    System.err.println();
+                }*/
+
+                
+            }
+        }
+            
+           
+
+           
            
           
 
