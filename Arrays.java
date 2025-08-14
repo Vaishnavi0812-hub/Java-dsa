28372f327f330dfcabcfe61af4287ebd1dc2761a
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-14 13:30:12 +0530

    arrays

--- Arrays.java
+++ Arrays.java
@@ -1,6 +1,56 @@
 import java.util.*;
 public class Arrays{
-    public static void maxSubarraySum(int numbers[]){
+    public static int trappedRainWater(int height[]){
+        int n=height.length;
+        //Calculate leftMax boundary
+        int leftMax[]=new int[n];
+        leftMax[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftMax[i]=Math.max(height[i], leftMax[i-1]);
+        }
+
+        //Calculate rightMax boundary
+        int rightMax[]=new int[n];
+        rightMax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightMax[i]=Math.max(height[i],rightMax[i+1]);
+        }
+        int trappedWater=0;
+        //loop
+        for(int i=0;i<n;i++){
+            //waterLevel=min(leftMax bound, rightMax bound)
+            int waterLevel=Math.min(leftMax[i], rightMax[i]);
+            //trappedWater=waterlevel-height[i]
+            trappedWater+=waterLevel-height[i];
+        }
+        return trappedWater;
+               
+    }
+    public static void main(String args[]){
+        int height[]={4,2,0,6,3,2,5};
+        System.out.println(trappedRainWater(height));
+    }
+}
+    /*public static void kadanes(int numbers[]){
+        int cs=0;
+        int ms=Integer.MIN_VALUE;
+
+        for(int i=0;i<numbers.length;i++){
+            cs=cs+numbers[i];
+            if(cs<0){
+                cs=0;
+            }
+            ms=Math.max(cs,ms);
+        }
+        System.out.println("Max subarray sum is " + ms);
+
+    }
+    public static void main(String args[]){
+        int numbers[]={-2,-3,-4,-1,-2,-1,-5,-3};
+        kadanes(numbers);
+    }
+}*/
+    /*public static void maxSubarraySum(int numbers[]){
         int currSum=0;
         int maxSum=Integer.MIN_VALUE;
         int prefix[]=new int[numbers.length];
@@ -31,8 +81,8 @@ public class Arrays{
         maxSubarraySum(numbers);
 
 
-    }
-}
+    }*/
+
  
     /*public static void printSubArrays(int numbers[]){
         int tp=0;
