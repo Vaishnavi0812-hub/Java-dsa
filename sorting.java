64b7c9762cc42c594aeccdc76d718cdc53d334c8
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-16 09:08:10 +0530

    sorting

--- sorting.java
+++ sorting.java
@@ -1,5 +1,28 @@
 public class Sorting{
-    public static void bubbleSort(int arr[]){
+    public static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minPos=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minPos]>arr[j]){
+                    minPos=j;
+                }
+            }
+            //swap
+            int temp=arr[minPos];
+            arr[minPos]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        selectionSort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    
+    }
+}
+    /*public static void bubbleSort(int arr[]){
         for(int turn=0;turn<arr.length-1;turn++){
             for(int j=0;j<arr.length-1-turn;j++){
                 if(arr[j]>arr[j+1]){
@@ -9,17 +32,20 @@ public class Sorting{
                 }
             }
         }
-    }
-    public static void printArr(int arr[]){
+    }*/
+    /*public static void printArr(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
-    }
-    public static void main(String args[]){
+    }*/
+    /*public static void main(String args[]){
         int arr[]={5,4,1,3,2};
         bubbleSort(arr);
-        printArr(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        //printArr(arr);
 
     }
     
-}
+}*/
