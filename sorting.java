82b93a7469998195097c49eb8cb7b4d82c8c8e21
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-17 08:45:57 +0530

    sorting

--- sorting.java
+++ sorting.java
@@ -1,6 +1,26 @@
-public class Sorting{
-    public static void selectionSort(int arr[]){
-        for(int i=0;i<arr.length-1;i++){
+import java.util.Arrays;
+public class sorting{
+     public static void main(String args[]){
+        int arr[]={5,3,4,2,1};
+        Arrays.sort(arr);
+    }
+
+}
+   
+
+    /*public static void insertionSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            //find out correct position to insert
+            while(prev>=0 && arr[prev]>curr ){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            //inserion
+            arr[prev+1]=curr;
+        }*/
+        /*for(int i=0;i<arr.length-1;i++){
             int minPos=i;
             for(int j=i+1;j<arr.length;j++){
                 if(arr[minPos]>arr[j]){
@@ -11,17 +31,18 @@ public class Sorting{
             int temp=arr[minPos];
             arr[minPos]=arr[i];
             arr[i]=temp;
-        }
-    }
-    public static void main(String[] args) {
+        }*/
+    
+    /*public static void main(String[] args) {
         int arr[]={5,4,1,3,2};
-        selectionSort(arr);
+        insertionSort(arr);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
     
-    }
-}
+    }*/
+    
+
     /*public static void bubbleSort(int arr[]){
         for(int turn=0;turn<arr.length-1;turn++){
             for(int j=0;j<arr.length-1-turn;j++){
