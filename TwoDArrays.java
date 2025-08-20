3130786e07462f16b05d69bdd7c6de0ef0227ed6
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-20 13:24:58 +0530

    2D Arrays

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -1,6 +1,52 @@
 import java.util.*;
 public class TwoDArrays {
-    public static boolean search(int matrix[][], int key){
+    public static void spiralMatrix(int matrix[][]){
+        int startRow=0;
+        int endRow=matrix.length-1;
+        int startCol=0;
+        int endCol=matrix[0].length-1;
+
+        while(startRow<=endRow && startCol<=endCol){
+            //top
+            for(int j=startCol;j<=endCol;j++){
+               System.out.print(matrix[startRow][j] + " ");
+            }
+            //right
+            for(int i=startRow+1;i<=endRow;i++){
+                System.out.print(matrix[i][endCol] + " ");
+            }
+            //bottom
+            for(int j=endCol-1;j>=startCol;j--){
+                if(startRow==endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j] + " ");
+            }
+            //left
+            for(int i=endRow-1;i>=startRow+1;i--){
+                if(startCol==endCol){
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+            }
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+        System.out.println();
+        
+
+        
+    }
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4},
+                     {5,6,7,8},
+                     {9,10,11,12},
+                     {13,14,15,16}};
+    spiralMatrix(matrix);
+    }
+    /*public static boolean search(int matrix[][], int key){
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==key){
@@ -31,7 +77,7 @@ public class TwoDArrays {
             System.out.println();
         }
         search(matrix, 5);
-    }
+    }*/
     
     
 }
