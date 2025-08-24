3bbfba34c0fe8aa1fcea608f0d38651ae7fa8863
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-08-24 09:45:43 +0530

    Strings

--- Strings.java
+++ Strings.java
@@ -1,15 +1,58 @@
 import java.util.*;
 public class Strings{
+    public static String compress(String str){
+        StringBuilder sb=new StringBuilder("");
+
+        for(int i=0;i<str.length();i++){
+            Integer count=1;
+
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+
+    }
     public static void main(String args[]){
-        
+        String str="aaabbcccdd";
+        System.out.println(compress(str));
+
+    }
+}
+    /*public static String toUpperCase(String str){
+        StringBuilder sb=new StringBuilder("");
+
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String args[]){
+        String str="hi, i am vaishnavi";
+        System.out.println(toUpperCase(str));*/
+
         /*StringBuilder sb=new StringBuilder("");
         for(char ch='a';ch<='z';ch++){
             sb.append(ch);
         }
         System.out.println(sb.length());*/
 
-    }
-}
+    
+
         /*String fruits[]={"apple", "banana", "mango"};
         String largest=fruits[0];
 
