5e150a68045728e665caed4879d5495f25bf4582
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-01 08:17:50 +0530

    Bit Manipulation

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -1,10 +1,22 @@
 import java.util.*;
 public class bitmanipulation{
+    public static void oddOrEven(int n){
+        int bitMask=1;
+        if((n& bitMask)==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
     public static void main(String args[]){
         //System.out.println((5&6));
         //System.out.println((5|6));
-        System.out.println(~0);
-        System.out.println((5<<2));
-        System.out.println((6>>1));
+        //System.out.println(~0);
+        //System.out.println((5<<2));
+        //System.out.println((6>>1));
+        oddOrEven(3);
+        oddOrEven(6);
+
+
     }
 }
