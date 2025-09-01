a90897e9263ab922e06ac03c497fc3c2a297f661
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-01 08:00:05 +0530

    Bit Manipulation

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -1,7 +1,10 @@
 import java.util.*;
 public class bitmanipulation{
     public static void main(String args[]){
-        System.out.println((5&6));
+        //System.out.println((5&6));
         //System.out.println((5|6));
+        System.out.println(~0);
+        System.out.println((5<<2));
+        System.out.println((6>>1));
     }
 }
