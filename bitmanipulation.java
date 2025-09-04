869aa56800027b7acb10c565d8ffe58fe186bb1e
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-04 20:51:13 +0530

    Bit Manipulation

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class bitmanipulation{
-    public static int clearRange(int n,int i,int j){
+    /*public static int clearRange(int n,int i,int j){
         int a=((~0)<<(j+1));
         int b=((1<<i) - 1);
         int bitMask=a|b;
         return n&bitMask;
-    }
+    }*/
     public static void main(String args[]){
         System.out.println(clearRange(10, 2, 4));
     
