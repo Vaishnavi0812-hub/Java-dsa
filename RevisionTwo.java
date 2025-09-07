fc5268231a59a50c8d6568d6cf7806816dbf4d58
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-07 23:12:10 +0530

    Revision 1

--- RevisionTwo.java
+++ RevisionTwo.java
@@ -7,7 +7,40 @@ public class RevisionTwo {
         }
         return -1;
     }*/
+    /*public static int largestSmallest(int numbers[]){
+        int largest=Integer.MIN_VALUE; 
+        int smallest=Integer.MAX_VALUE;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]>largest){
+                largest=numbers[i];
+            }
+            if(numbers[i]<smallest){
+                smallest=numbers[i];
+            }
+        }
+        System.out.println("The smallest is " + smallest);
+        return largest;
+    }*/
+    /*public static int binarySearch(int numbers[],int key){
+        int start=0, end=numbers.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(numbers[mid]==key){
+                return mid;
+            }
+            if(numbers[mid]>key){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }*/
     public static void main (String args[]){
+        int numbers[]={2,4,6,8,10,12,14};
+        int key=10;
+        System.out.println("Index of key is  " + binarySearch(numbers,key));
+
         /*int numbers[]={2,4,6,8,10,12};
         int key=10;
         System.out.println(linearSearch(numbers,key));*/
