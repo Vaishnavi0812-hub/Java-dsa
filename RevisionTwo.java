ffd3fcd37536807829c105ce1584e77e22c2fc7b
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-08 14:01:10 +0530

    Revision 1

--- RevisionTwo.java
+++ RevisionTwo.java
@@ -58,7 +58,7 @@ public class RevisionTwo {
         }
         System.out.println("Total no of pairs is " + tp);
     }*/
-    public static void printSubArr(int arr[]){
+    /*public static void printSubArr(int arr[]){
         int tp=0;
         for(int i=0;i<arr.length;i++){
             int sum=0;
@@ -75,11 +75,35 @@ public class RevisionTwo {
         }
         System.out.println("Total no of subarrays are " + tp);
                                     
+    }*/
+    public static void maxSubarraySum(int arr[]){
+        int currSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        //create a prefix array
+        int prefix[]=new int[arr.length];
+        //caculate prefix array
+        prefix[0]=arr[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=arr[i] + prefix[i-1];
+        }
+        for(int i=0;i<arr.length;i++){
+            int start=i;
+            for(int j=i;j<arr.length;j++){
+                int end=j;
+                currSum=start==0?prefix[end]:prefix[end]-prefix[start-1];
+                if(currSum>maxSum){
+                    maxSum=currSum;
+                }
+            }
+        }
+        System.out.println("max sum is " + maxSum);
     }
 
     public static void main (String args[]){
-        int arr[]={2,4,6,8,10};
-        printSubArr(arr);
+        int arr[]={1,-2,6,-1,3};
+        maxSubarraySum(arr);
+        /*int arr[]={2,4,6,8,10};
+        printSubArr(arr);*/
 
         /*int numbers[]={2,4,6,8,10,12,14};
         reverseArray(numbers);
