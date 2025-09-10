95c76ace60b05f14042e12c90c8aa775118fd2d5
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-10 14:11:27 +0530

    Revision 2

--- RevisionTwo.java
+++ RevisionTwo.java
@@ -184,7 +184,7 @@ public class RevisionTwo {
             }
         
     }*/
-    public static boolean searchMatrix(int matrix[][],int key){
+    /*public static boolean searchMatrix(int matrix[][],int key){
          for(int i=0;i<matrix.length;i++){
                 for(int j=0;j<matrix[0].length;j++){
                      if(matrix[i][j]==key){
@@ -196,9 +196,25 @@ public class RevisionTwo {
             System.out.println("Key not found");
             return false;
 
-    }
+    }*/
      public static void main (String args[]){
-        int matrix[][]=new int[3][3];
+        int matrix[][]={{2,3,7} , {5,6,7}};
+        int row=2, col=3;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int transpose[][]=new int[col][row];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                transpose[j][i]=matrix[i][j];
+                System.out.print(transpose[j][i] + " ");
+            }
+            System.out.println();
+        }
+        /*int matrix[][]=new int[3][3];
         int n=matrix.length;
         int m=matrix[0].length;
 
@@ -214,7 +230,7 @@ public class RevisionTwo {
                 }
                 System.out.println();
             }
-            searchMatrix(matrix, 5);
+            searchMatrix(matrix, 5);*/
         
 
         /*int arr[]={1,4,1,3,2,4,3,7};
