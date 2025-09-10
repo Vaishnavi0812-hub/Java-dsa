1ced88e77897dd6c86b150c58336b6fa7fdf472e
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-10 14:27:40 +0530

    Revision 2

--- RevisionTwo.java
+++ RevisionTwo.java
@@ -197,8 +197,27 @@ public class RevisionTwo {
             return false;
 
     }*/
+    public static void printMatrix(int matrix[][]){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
      public static void main (String args[]){
-        
+        int row=2, col=3;
+        int matrix[][]={{1,2,3}, {4,5,6}};
+        printMatrix(matrix);
+
+        int transpose[][]=new int[col][row];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                transpose[j][i]=matrix[i][j];
+            }
+        }
+        printMatrix(transpose);
+
         /*int matrix[][]={{2,3,7} , {5,6,7}};
         int row=2, col=3;
         for(int i=0;i<matrix.length;i++){
