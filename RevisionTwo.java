b5055dad7e3d993f74034187c8f8ef691392de49
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-11 20:20:12 +0530

    Revision 2

--- RevisionTwo.java
+++ RevisionTwo.java
@@ -230,7 +230,25 @@ public class RevisionTwo {
         return (float)Math.sqrt(X2+Y2);
 
     }*/
+    public static String toUpperCase(String str){
+        StringBuilder sb=new StringBuilder(" ");
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' '&& i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
      public static void main (String args[]){
+        String str="hi i am vaishnavi";
+        System.out.println(toUpperCase(str));
         /*String fruits[]={"apple", "mango", "banana"};
         String largest=fruits[0];
         for(int i=1;i<fruits.length;i++){
