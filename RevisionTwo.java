e03e26939de8e141426230d34b73cbe3c246395c
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-13 14:52:16 +0530

    Revision 2

--- RevisionTwo.java
+++ RevisionTwo.java
@@ -254,14 +254,27 @@ public class RevisionTwo {
             System.out.println("Odd");
         }
     }*/
-    public static int updateIthBit(int n, int i,int newBit){
-        n=clearIthBit(n,i);
-        int bitMask=newBit<<i;
-        return n|bitMask;
+    /*public static int clearRange(int n, int i,int j){
+        int a=((~0)<<(j+1));
+        int b=(1<<i)-1;
+        int bitMask=a|b;
+        return n&bitMask;
         
+    }*/
+    public static int fastExpo(int a,int n){
+        int ans=1;
+        while(n>0){
+            if((n&1)==1){
+                ans=ans*a;
+            }
+            a=a*a;
+            n=n>>1;
+        }
+        return ans;
     }
      public static void main (String args[]){
-        System.out.println(clearIthBit(10, 1));
+        System.out.println(fastExpo(3, 5));
+        
        
 
         /*String str="hi i am vaishnavi";
