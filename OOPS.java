acab923a94c37a014d6c73bd4752f503d313b72d
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-14 13:54:24 +0530

    OOPS

--- OOPS.java
+++ OOPS.java
@@ -1,8 +1,6 @@
 public class OOPS {
     public static void main(String args[]){
         Pen p1=new Pen();
-        p1.setColor("Blue");
-        System.out.println(p1.color);
 
     }
     
@@ -12,10 +10,7 @@ class Pen{
     String color;
     int tip;
 
-    void setColor(String newColor){
-        color=newColor;
-    }
-    void setTip(int newTip){
-        tip=newTip;
+    Pen(){
+        System.out.println("The color is blue");
     }
 }
