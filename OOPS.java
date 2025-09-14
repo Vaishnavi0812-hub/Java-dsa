f2545af8f4c7f5ac44343e4931a6f05dacf1b886
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-14 13:53:15 +0530

    OOPS

---
+++ OOPS.java
@@ -0,0 +1,21 @@
+public class OOPS {
+    public static void main(String args[]){
+        Pen p1=new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+
+    }
+    
+}
+class Pen{
+    //prop + func
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color=newColor;
+    }
+    void setTip(int newTip){
+        tip=newTip;
+    }
+}
