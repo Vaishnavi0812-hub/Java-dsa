cb27f6eeb2fa7557a5e6d157750d065766be21b8
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-14 16:09:52 +0530

    OOPS

--- OOPS.java
+++ OOPS.java
@@ -1,11 +1,16 @@
 public class OOPS {
     public static void main(String args[]){
+    Fish tuna=new Fish();
+    tuna.eat();
+
+}
+    /*public static void main(String args[]){
         Pen p1=new Pen();
 
-    }
+    }*/
     
 }
-class Pen{
+/*class Pen{
     //prop + func
     String color;
     int tip;
@@ -13,4 +18,23 @@ class Pen{
     Pen(){
         System.out.println("The color is blue");
     }
+}*/
+
+
+class Animal{
+    String color;
+
+    void eat(){
+        System.out.println("eats");
+    }
+    void breathe(){
+        System.out.println("breathes");
+    }
+}
+class Fish extends Animal{
+    int fins;
+    void swim(){
+        System.out.println("swims");
+    }
+
 }
