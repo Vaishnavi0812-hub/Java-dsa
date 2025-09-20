113322db2c51e0f4bfff3d19fdcabefc79cc47a3
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-20 19:06:38 +0530

    Recursion

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -147,12 +147,21 @@ public class RecursionBasics {
 
         return totWays;
     }*/
-    public static void binaryStrings(int n, int lastPlace, StringBuilder str){
+    public static void binaryStrings(int n, int lastPlace, String str){
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
         if(lastPlace==0){
-            binaryStrings(n-1, 0, str.append);
+            binaryStrings(n-1, 0, str+"0");
+            binaryStrings(n-1, 1, str+"1");
+        } else{
+            binaryStrings(n-1, 0, str+"0");
+
         }
     }
     public static void main(String args[]){
+        binaryStrings(3, 0, "");
         //System.out.println(friendsPair(3));
         //String str="appnnacollege";
         //removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
