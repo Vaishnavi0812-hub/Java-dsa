3c6aaf401c87d3985c4b742a42cbf337413ac5c1
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-25 16:52:27 +0530

    Divide and Conquer

--- DivideAndConquer.java
+++ DivideAndConquer.java
@@ -1,5 +1,5 @@
 public class DivideAndConquer {
-    public static void mergeSort(int arr[], int si, int ei){
+    /*public static void mergeSort(int arr[], int si, int ei){
         if(si>=ei){
             return;
         }
@@ -35,11 +35,37 @@ public class DivideAndConquer {
         for(k=0, i=si;k<temp.length;k++, i++){
             arr[i]=temp[k];
         }
+    }*/
+    public static void quickSort(int arr[], int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int pIdx=partition(arr,si,ei);
+        quickSort(arr,si,pIdx-1);
+        quickSort(arr,pIdx+1,ei);
     }
+    public static int partition(int arr[], int si, int ei){
+        int pivot=arr[ei];
+        int i=si-1;
 
+        for(int j=si;j<ei;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        i++;
+        int temp=pivot;
+        arr[ei]=arr[i];
+        arr[i]=temp;
+        return i;
+
+    }
     public static void main(String args[]){
         int arr[]={6,3,9,5,2,8};
-        mergeSort(arr,0,arr.length-1);
+        quickSort(arr,0,arr.length-1);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
