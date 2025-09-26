cd9646978fe66c5dcbff765817c898165baad89e
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-09-26 17:20:53 +0530

    Divide and Conquer

--- DivideAndConquer.java
+++ DivideAndConquer.java
@@ -133,7 +133,7 @@ public class DivideAndConquer {
     }*/
 
     // driver
-    public static int getInvCount(int arr[]){
+    /*public static int getInvCount(int arr[]){
         int invCount=0;
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
@@ -143,10 +143,51 @@ public class DivideAndConquer {
             }
         }
         return invCount;
+    }*/
+    public static int merge(int arr[], int left,int mid, int right){
+        int invCount=0;
+        int i=left, j=mid+1, k=0;
+        int temp[]=new int[right-left+1];
+
+        while(i<=mid && j<=right){
+            if(arr[i]<=arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }else{
+                temp[k]=arr[j];
+                invCount+=(mid-i+1);
+                j++;
+            }
+            k++;
+        }
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=right){
+            temp[k++]=arr[j++];
+        }
+
+        for(k=0, i=left;k<temp.length;k++,i++){
+            arr[i]=temp[k];
+        }
+        return invCount;
     }
+    public static int mergeSort(int arr[], int left,int right){
+        int invCount=0;
+        if(right>left){
+            int mid=left+(right-left)/2;
+            invCount+=mergeSort(arr,left,mid);
+            invCount+=mergeSort(arr,mid+1,right);
+            invCount+=merge(arr,left,mid,right);
+        }
+        return invCount;
+    }
+
+
     public static void main(String[] args) {
         int arr[]={1,20,6,4,5};
-        System.out.println("Inversion count is " + getInvCount(arr));
+        System.out.println(mergeSort(arr,0,arr.length-1));
+        
         /*String[] arr = { "sun", "earth", "mars", "mercury" };
         // handle empty array safely
         if (arr.length == 0) {
