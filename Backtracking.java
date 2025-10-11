d785de01c8ac3f04794c4282c421c6f02280d630
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-10-11 13:48:07 +0530

    Backtracking

--- Backtracking.java
+++ Backtracking.java
@@ -1,5 +1,5 @@
 public class Backtracking {
-    public static void changeArr(int arr[], int i, int val){
+    /*public static void changeArr(int arr[], int i, int val){
         if(i==arr.length){
             printArr(arr);
             return;
@@ -19,6 +19,25 @@ public class Backtracking {
         changeArr(arr, 0, 1);
         printArr(arr);
         
+    }*/
+    public static void subset(String str, String ans, int i){
+        if(i==str.length()){
+            if(ans.length()==0){
+                System.out.println("null");
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+        //yes
+        subset(str, ans+str.charAt(i), i+1);
+        //no
+        subset(str, ans, i+1);
+    }
+
+    public static void main (String args[]){
+        String str="abc";
+        subset(str, "", 0);
     }
     
 }
