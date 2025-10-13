76bf6e6437878790560e5851e965122c22ebbece
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-10-13 08:28:20 +0530

    Backtracking

--- Backtracking.java
+++ Backtracking.java
@@ -39,7 +39,7 @@ public class Backtracking {
         String str="abc";
         subset(str, "", 0);
     }*/
-    public static void findPermutations(String str, String ans){
+    /*public static void findPermutations(String str, String ans){
 
         if(str.length()==0){
             System.out.println(ans);
@@ -55,6 +55,39 @@ public class Backtracking {
     public static void main(String args[]){
         String str="abc";
         findPermutations(str,"");
+    }*/
+    public static void nQueens(char board[][], int row){
+        if(row==board.length){
+            printBoard(board);
+            return;
+        }
+        for(int j=0;j<board.length;j++){
+            board[row][j]='Q';
+            nQueens(board, row+1);
+            board[row][j]='x';
+        }
     }
+    public static void printBoard(char board[][]){
+        System.out.println("-----chess board------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+             System.out.println();
+        }
+       
+    }
+    public static void main(String args[]){
+        int n=2;
+        char board[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='x';
+            }
+        }
+        nQueens(board,0);
+    }
+
+    
     
 }
