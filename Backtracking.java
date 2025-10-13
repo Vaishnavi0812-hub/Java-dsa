0010a8c15766d67b11181959c1c990f6aad22924
Vaishnavi0812-hub <vaishnavi08r@gmail.com>
2025-10-13 09:03:58 +0530

    Backtracking

--- Backtracking.java
+++ Backtracking.java
@@ -56,15 +56,39 @@ public class Backtracking {
         String str="abc";
         findPermutations(str,"");
     }*/
+    public static boolean isSafe(char board[][], int row, int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        //left diagonal
+        for(int i=row-1, j=col-1; i>=0 && j>=0;i--,j--){
+             if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //right diagonal
+        for(int i=row-1, j=col+1; i>=0 && j<board.length;i--,j++){
+             if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+
+    }
     public static void nQueens(char board[][], int row){
         if(row==board.length){
             printBoard(board);
             return;
         }
         for(int j=0;j<board.length;j++){
-            board[row][j]='Q';
-            nQueens(board, row+1);
-            board[row][j]='x';
+            if(isSafe(board, row, j)){
+                board[row][j]='Q';
+                nQueens(board, row+1);
+                board[row][j]='x';
+            }
         }
     }
     public static void printBoard(char board[][]){
@@ -78,7 +102,7 @@ public class Backtracking {
        
     }
     public static void main(String args[]){
-        int n=2;
+        int n=4;
         char board[][]=new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
