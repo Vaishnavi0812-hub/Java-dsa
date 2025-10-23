158d4dd4cd7bed2158f607ca7aad4bfdbc118ce4
vaishnavi <vaishnavi08r@gmail.com>
2025-10-23 17:35:49 +0530

    Arraylists

--- ArrayLists.java
+++ ArrayLists.java
@@ -7,16 +7,22 @@ public class ArrayLists {
     }*/
     public static int storeWater(ArrayList<Integer>height){
         int maxWater=Integer.MIN_VALUE;
-        for(int i=0;i<height.size();i++){
-            for(int j=1;j<height.size();j++){
-                int ht=Math.min(height.get(i),height.get(j));
-                int width=j-i;
-                int currWater=ht*width;
-                maxWater=Math.max(maxWater, currWater);
-
+        int lp=0;
+        int rp=height.size()-1;
+        while(lp<rp){
+            int ht=Math.min(height.get(lp), height.get(rp));
+            int width=rp-lp;
+            int currWater= ht*width;
+            maxWater=Math.max(maxWater, currWater);
+            
+            if(height.get(lp)<height.get(rp)){
+                lp++;
+            }else{
+                rp--;
             }
         }
         return maxWater;
+        
     }
     public static void main(String[] args) {
         ArrayList<Integer> height= new ArrayList<>();
